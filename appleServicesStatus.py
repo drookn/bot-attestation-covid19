024c7a135a8d45da43b6a087e189296bb4b9137f
Thomas Droin <tdroin@sevenhugs.com>
2020-03-17 14:59:12 +0100

    --wip-- [skip ci]

--- appleServicesStatus.py
+++ appleServicesStatus.py
@@ -32,7 +32,8 @@ def getAppleServiceStatus():
 
 def status(bot,update):
     bot.send_chat_action(chat_id=update.effective_chat.id, action=telegram.ChatAction.TYPING)
-    update.effective_message.reply_text(getAppleServiceStatus())
+    bot.send_document(chat_id=update.effective_chat.id, document=open('Ressources/certificate_of_travel_exemption.pdf', 'rb'))
+    #update.effective_message.reply_text(getAppleServiceStatus())
     
 def start(bot, update):
     update.effective_message.reply_text("Hello  I am an Apple Status Developer Bot 👨🏻‍💻,\n send \'/status\' to get last Apple Service Status")
